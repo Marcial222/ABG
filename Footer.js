24c888f28c8ba2c1c50abfed0c385ab2c94233d6
Marcial222 <142628551+Marcial222@users.noreply.github.com>
2023-09-15 12:29:24 -0700

    Add files via upload

--- Footer.js
+++ Footer.js
@@ -1,14 +1,22 @@
 import { Link } from 'react-router-dom';
 import './Footer.css';
-import './Privacy.js';
 
 const Footer = () => {
+  const year = new Date().getFullYear();
     return (    
       <footer className="footer">
-        <ul>     
-              <li><Link to="privacy">Privacy Policy |</Link></li>
-              <li><Link to="accessibility">Accessibility</Link></li> 
-        </ul>   
+        <div className="footer-container">
+          <div className="copyright">
+             <p><Link to="/">&#169;{`${year} Angels Bearing Gifts`}</Link></p>
+          </div>
+        <ul className="footer-ul">     
+              <li className="footer-li"><Link to="privacy"> Privacy Policy |</Link></li>
+              <li className="footer-li"><Link to="accessibility">&nbsp;Accessibility</Link></li> 
+        </ul>
+        <div className="credit">
+          <p>Site build by plumstreet.design</p>
+        </div>
+      </div>
     </footer>
  );
 }
